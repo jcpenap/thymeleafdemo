ec4a0c2e8bc7dd49cdcc8d225fda90c6abb7e5f8
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-16 17:38:23 -0500

    Redering object Data and multiple records

--- src/main/java/com/bahrath/springboot/thymeleaf/controllers/HelloController.java
+++ src/main/java/com/bahrath/springboot/thymeleaf/controllers/HelloController.java
@@ -1,8 +1,12 @@
 package com.bahrath.springboot.thymeleaf.controllers;
 
+import com.bahrath.springboot.thymeleaf.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class HelloController {
@@ -12,10 +16,38 @@ public class HelloController {
         return "Hello";
     }
 
-    @RequestMapping("sendData")
+    @RequestMapping("/sendData")
     public ModelAndView sendData() {
         ModelAndView modelAndView = new ModelAndView("data");
         modelAndView.addObject("message", "Take up one idea and make it your life");
+        return modelAndView;
+    }
+
+    @RequestMapping("/student")
+    public ModelAndView getStudent() {
+        ModelAndView modelAndView = new ModelAndView("student");
+        Student student = new Student();
+        student.setName("Jhon");
+        student.setScore(80);
+        modelAndView.addObject("student", student);
+        return modelAndView;
+    }
+
+    @RequestMapping("/students")
+    public ModelAndView getStudents() {
+        ModelAndView modelAndView = new ModelAndView("students");
+
+        Student student = new Student();
+        student.setName("Jhon");
+        student.setScore(80);
+
+        Student student2 = new Student();
+        student2.setName("Bob");
+        student2.setScore(90);
+
+        List<Student> students = Arrays.asList(student, student2);
+
+        modelAndView.addObject("students", students);
         return modelAndView;
     }
 }
---
+++ src/main/java/com/bahrath/springboot/thymeleaf/model/Student.java
@@ -0,0 +1,13 @@
+package com.bahrath.springboot.thymeleaf.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Student {
+
+    private String name;// name of the student
+    private int score;// score of the student
+
+}
