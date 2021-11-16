30214d448814b0887992995861ec2608739f0b34
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-16 14:38:54 -0500

    first commit

---
+++ src/test/java/com/bahrath/springboot/thymeleaf/ThymeleafdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bahrath.springboot.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
